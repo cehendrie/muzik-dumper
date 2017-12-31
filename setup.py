a64211e10284c4da334762ec68023c355cb26532
cehendrie <cehendrie@icloud.com>
2017-12-31 11:21:01 -0600

    Fix script executable
    Added a .pth file to the build to add 'muzikdumpster' to the
    PYTHONPATH / sys.path. .pth file is copied to site-packages using
    the shutil utility, but really should be done by setup() method.

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
+import shutil
+from distutils import sysconfig
 from setuptools import setup
+
+# PYTHONPATH hack!!!
+# Needed for this script to execute the application successfully.
+# 'muzikdumpster' package needs to be added to the path (sys.path).
+# Using the .pth convention to add the package sys.path.
+# The setup(data_files) parameter should be doing the copy but isn't working.
+# See: https://stackoverflow.com/questions/2145779/setup-py-installing-just-a-pth-file
+site_packages_path = sysconfig.get_python_lib()
+shutil.copy('muzikdumpster.pth', site_packages_path)
+
 
 def readme():
     with open('README.rst') as f:
         return f.read()
+
 
 setup(name='muzik-dumpster',
       version='0.0.1',
@@ -17,4 +30,5 @@ setup(name='muzik-dumpster',
       ],
       include_package_data=True,
       zip_safe=False,
-      scripts=['bin/muzik-dumpster'])
+      scripts=['bin/muzik-dumpster'],
+      data_files=[(site_packages_path, ['muzikdumpster.pth'])])
